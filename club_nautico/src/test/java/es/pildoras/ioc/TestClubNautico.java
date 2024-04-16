f395775a87016630323e9abd00bcda17569826eb
Jose Maria Valdes Ortiz (ext) <jose.valdes.external@eviden.com>
2024-04-16 13:28:27 +0200

    jpa_tarea_2.7

---
+++ club_nautico/src/test/java/es/pildoras/ioc/TestClubNautico.java
@@ -0,0 +1,14 @@
+package es.pildoras.ioc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestClubNautico {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
